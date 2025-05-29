52259d3324bd320e074486acda9c4e1612f4fb46
Jaakko Sirén <jaakko.s@iki.fi>
2025-05-30 00:37:22 +0300

    Packaging fixes

--- scripts/prepare-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to prepare data files for packaging
-# This copies data/ to share/ulauncher/ which is what pdm-backend expects
-
-rm -rf share/
-mkdir -p share/ulauncher
-cp -r data/* share/ulauncher/
-echo "Data files prepared in share/ulauncher/"
